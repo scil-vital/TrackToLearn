943569a447db900fbdebcdb2ff816352006eeef9
Antoine Théberge <thea1603@gra-login2.graham.sharcnet>
2023-12-10 12:55:38 -0500

    Merge

--- scripts/rl_test_tractoinferno_exp2.sh
+++ scripts/rl_test_tractoinferno_exp2.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# seeds=(1111 2222 3333 4444 5555)
-
-
 sub=$3
 rng_seed=$4
 prob=$5
@@ -11,9 +8,10 @@ prob=$5
 # do
 
 experiment_path=${TRACK_TO_LEARN_DATA}/experiments/
+tracking_folder=${LOCAL_TRACK_TO_LEARN_DATA}/experiments/$1/$2/$rng_seed/tractoinferno/${sub}
 
 data_path=${TRACK_TO_LEARN_DATA}/datasets/tractoinferno/${sub}
-file=$6
+file=${tracking_folder}/tractogram_${prob}_tractoinferno_${sub}_20.trk
 
 # if [[ -f $file ]]; then
 #   echo $file "exists"
@@ -21,12 +19,13 @@ file=$6
 # fi
 
 echo "Tracking" ${sub} "seed" ${rng_seed}
+mkdir -p $tracking_folder
 ttl_track.py \
   ${data_path}/fodf/${sub}__fodf.nii.gz \
   ${data_path}/maps/${sub}__interface.nii.gz \
   ${data_path}/mask/${sub}__mask_wm.nii.gz \
   $file \
-  --interface --binary=0.1 \
-  --agent ${experiment_path}/$1/$2/$rng_seed/model \
+  --interface \
+  --policy ${experiment_path}/$1/$2/$rng_seed/model \
   --hyper ${experiment_path}/$1/$2/$rng_seed/model/hyperparameters.json \
-  --npv 20 --n_actor 100000 --compress 0.2 --prob $prob -f
+  --npv 20 --n_actor 50000 --compress 0.2 --prob $prob -f
