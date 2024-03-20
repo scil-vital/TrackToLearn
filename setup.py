0c1228e3379c999c12a05c20c3a2c1af4d386107
AntoineTheb <antoine.theberge@usherbrooke.ca>
2024-03-20 17:53:49 +0000

    FIX: remove reqs

--- setup.py
+++ setup.py
@@ -65,7 +65,6 @@ setup(
     # for example:
     # $ pip install -e .[dev,test]
     extras_require={
-        'dev': ["Cython", "numpy", "nibabel", "hdf5"],
     },
 
     # If there are data files included in your packages that need to be
